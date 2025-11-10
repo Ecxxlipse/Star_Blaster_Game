f8d5643ab4846282088dbb6f179c15db6e57c906
Ecxxlipse <arnold219@morrisville.edu>
2025-11-10 13:43:44 -0500

    Setting up movement and viewpoint Boundaries

---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,48 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Playercontroller : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float leftBoundPadding;
+    [SerializeField] float rightBoundPadding;
+    [SerializeField] float upBoundPadding;
+    [SerializeField] float downBoundPadding;
+    InputAction moveAction;
+
+    Vector3 moveVector;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+
+        InitBounds();
+
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+    void MovePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
+
+        newPos.x = Math.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Math.Clamp(newPos.y, minBounds.y + downBoundPadding, maxBounds.y - upBoundPadding);
+
+        transform.position = newPos;
+    }
+}
